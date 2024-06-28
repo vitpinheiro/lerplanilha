4ec59394ef291c5311ca9b0394d2102ab9df94e4
vitpinheiro <vitoriapinheiro775@gmail.com>
2024-06-28 16:05:32 -0300

    UP

--- teste2.py
+++ teste2.py
@@ -41,9 +41,10 @@ def main_page():
     if uploaded_file is not None:
         guia = st.checkbox("Filtro Guia", value=True)
         if guia:
-            guide_values = st.text_area('Digite os valores para a coluna "Guia" (separados por vírgulas)').split(',')
+            guide_values = st.text_input('Digite os valores para a coluna "Guia" (separados por vírgulas)').split(',')
 
         data = st.checkbox("Filtro Data", value=True)
+        date_range = None
         if data:
             date_range = st.date_input(
                 "Selecione o intervalo de datas para a coluna 'Dt item'",
@@ -61,7 +62,7 @@ def main_page():
                 st.download_button(
                     label="Baixar arquivo Excel",
                     data=output,
-                    file_name=f"resultado_filtrado_{datetime.today().strftime('%Y-%m-%d')}.xlsx",
+                    file_name=f"resultado_demonstrativo_{datetime.today().strftime('%Y-%m-%d')}.xlsx",
                     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                 )
 
@@ -77,8 +78,15 @@ def main_page():
     if uploaded_file2 is not None:
         if guide_values:
             df = pd.read_excel(uploaded_file2)
-            colunas_guia = ['GUIA_ATENDIMENTO', 'GUIA_CONTA', 'GIH_NUMERO']
-            df_filtered_guia = df[df.isin(colunas_guia).any(axis=1)]
+            # Remova pontos dos valores nas colunas relevantes
+            df['GUIA_ATENDIMENTO'] = df['GUIA_ATENDIMENTO'].apply(lambda x: str(x).replace('.', ''))
+            df['GUIA_CONTA'] = df['GUIA_CONTA'].apply(lambda x: str(x).replace('.', ''))
+            df['GIH_NUMERO'] = df['GIH_NUMERO'].apply(lambda x: str(x).replace('.', ''))
+            guide_values = [str(value).replace('.', '') for value in guide_values]
+
+            # Filtre as colunas relevantes com base nos valores de guia
+            df_filtered_guia = df[(df['GUIA_ATENDIMENTO'].isin(guide_values)) | (df['GUIA_CONTA'].isin(guide_values)) | (df['GIH_NUMERO'].isin(guide_values))]
+
             df_filtered_guia = df_filtered_guia[df_filtered_guia['CTH_NUM'] == 1]
             df_filtered_guia = df_filtered_guia[df_filtered_guia['GUIA_ATENDIMENTO'] == df_filtered_guia['GIH_NUMERO']]
             df_filtered2 = df_filtered_guia[['HSP_NUM', 'HSP_PAC', 'CTH_NUM', 'FAT_SERIE', 'FAT_NUM', 'GUIA_ATENDIMENTO', 'GUIA_CONTA', 'GIH_NUMERO']]
@@ -97,11 +105,9 @@ def main_page():
     else:
         st.write("Por favor, faça o upload do arquivo ATENDIMENTOS v3.xls.")
 
-
-
 def page_tratamento():
     st.image("LOGO.png", width=150)
-    st.write("TRATAMENTO:")
+    st.write("TRATAMENTO")
     st.header('Leitura e Filtro de Arquivo XLS')
 
     uploaded_file = st.file_uploader("Escolha um arquivo XLS/XLSX")
@@ -112,9 +118,10 @@ def page_tratamento():
     if uploaded_file is not None:
         guia = st.checkbox("Filtro Guia", value=True)
         if guia:
-            guide_values = st.text_area('Digite os valores para a coluna "Guia" (separados por vírgulas)').split(',')
+            guide_values = st.text_input('Digite os valores para a coluna "Guia" (separados por vírgulas)').split(',')
 
         data = st.checkbox("Filtro Data", value=True)
+        date_range = None
         if data:
             date_range = st.date_input(
                 "Selecione o intervalo de datas para a coluna 'Dt item'",
@@ -132,7 +139,7 @@ def page_tratamento():
                 st.download_button(
                     label="Baixar arquivo Excel",
                     data=output,
-                    file_name=f"resultado_filtrado_{datetime.today().strftime('%Y-%m-%d')}.xlsx",
+                    file_name=f"resultado_ATENDIMENTO_{datetime.today().strftime('%Y-%m-%d')}.xlsx",
                     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                 )
 
@@ -140,28 +147,27 @@ def page_tratamento():
         st.write(f'Tamanho do arquivo: {uploaded_file.size} bytes')
     else:
         st.write("Por favor, faça o upload de um arquivo XLS/XLSX.")
-    
+
     st.header('Upload e Filtragem do Arquivo ATENDIMENTOS')
-   
-    
     uploaded_file2 = st.file_uploader("Escolha o arquivo ATENDIMENTOS.xls", key="atendimentos")
 
     if uploaded_file2 is not None:
         if guide_values:
             df = pd.read_excel(uploaded_file2)
-
-
-            colunas_guia = ['GUIA_ATENDIMENTO', 'GIH_NUMERO', 'FAT_NUM']
+            # Remova pontos dos valores nas colunas relevantes
+            df['GUIA_ATENDIMENTO'] = df['GUIA_ATENDIMENTO'].apply(lambda x: str(x).replace('.', ''))
+            df['GIH_NUMERO'] = df['GIH_NUMERO'].apply(lambda x: str(x).replace('.', ''))
+            guide_values = [str(value).replace('.', '') for value in guide_values]
 
             df_filtered_guia = df[df['GUIA_ATENDIMENTO'].isin(guide_values) | df['GIH_NUMERO'].isin(guide_values)]
 
             df_filtered_guia = df_filtered_guia[df_filtered_guia['CTH_NUM'] == 0]
-            df_filtered_guia = df_filtered_guia[pd.notna(df_filtered_guia['FAT_NUM'] )]
+            df_filtered_guia = df_filtered_guia[pd.notna(df_filtered_guia['FAT_NUM'])]
             df_filtered_guia = df_filtered_guia[df_filtered_guia['GUIA_ATENDIMENTO'] == df_filtered_guia['GIH_NUMERO']]
             # Mostra as colunas filtradas de interesse
-            df_filtered2 = df_filtered_guia[['CTH_NUM', 'GUIA_ATENDIMENTO','GIH_NUMERO', 'FAT_NUM']]
-
+            df_filtered2 = df_filtered_guia[['HSP_NUM', 'HSP_PAC', 'CTH_NUM', 'FAT_SERIE', 'FAT_NUM', 'GUIA_ATENDIMENTO', 'GUIA_CONTA', 'GIH_NUMERO']]
             st.dataframe(df_filtered2)
+
             output2 = BytesIO()
             df_filtered2.to_excel(output2, index=False)
             output2.seek(0)
@@ -175,6 +181,7 @@ def page_tratamento():
             st.write("Por favor, insira um valor para a coluna 'Guia' no filtro acima.")
     else:
         st.write("Por favor, faça o upload do arquivo ATENDIMENTOS v3.xls.")
+
 
 # Navegação entre páginas
 st.sidebar.title("Navegação")
