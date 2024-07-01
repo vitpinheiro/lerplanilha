a09827bb8c5d76a95a81097192baa38220df1013
vitpinheiro <vitoriapinheiro775@gmail.com>
2024-07-01 14:14:00 -0300

    ok

--- teste2.py
+++ teste2.py
@@ -1,9 +1,7 @@
-# TESTE GERAL
 import pandas as pd
 import streamlit as st
 from io import BytesIO
 from datetime import datetime
-# from conexaobanco import connect_to_database, execute_sql_query
 
 # Função para ler e filtrar o arquivo XLS
 def read_and_filter_xls(xls_file, column_names, guide_values=None, date_range=None):
@@ -52,7 +50,11 @@ def main_page():
             )
 
         if st.button('Aplicar Filtros'):
-            df_filtered = read_and_filter_xls(uploaded_file, column_names, guide_values if guia else None, date_range if data else None)
+            # Ajustar os parâmetros para a função de leitura e filtragem
+            guide_values_to_use = guide_values if guia else None
+            date_range_to_use = date_range if data else None
+
+            df_filtered = read_and_filter_xls(uploaded_file, column_names, guide_values_to_use, date_range_to_use)
             if df_filtered is not None:
                 st.write('Tabela filtrada pelos valores selecionados:')
                 st.dataframe(df_filtered)
@@ -76,34 +78,38 @@ def main_page():
     uploaded_file2 = st.file_uploader("Escolha o arquivo ATENDIMENTOS.xls", key="atendimentos")
 
     if uploaded_file2 is not None:
+        # Leitura do arquivo ATENDIMENTOS
+        df = pd.read_excel(uploaded_file2)
+
+        # Remover pontos dos valores nas colunas relevantes
+        df['GUIA_ATENDIMENTO'] = df['GUIA_ATENDIMENTO'].apply(lambda x: str(x).replace('.', ''))
+        df['GUIA_CONTA'] = df['GUIA_CONTA'].apply(lambda x: str(x).replace('.', ''))
+        df['GIH_NUMERO'] = df['GIH_NUMERO'].apply(lambda x: str(x).replace('.', ''))
+        guide_values = [str(value).replace('.', '') for value in guide_values]
+
+        # Aplicar filtros conforme os valores das guias e data
         if guide_values:
-            df = pd.read_excel(uploaded_file2)
-            # Remova pontos dos valores nas colunas relevantes
-            df['GUIA_ATENDIMENTO'] = df['GUIA_ATENDIMENTO'].apply(lambda x: str(x).replace('.', ''))
-            df['GUIA_CONTA'] = df['GUIA_CONTA'].apply(lambda x: str(x).replace('.', ''))
-            df['GIH_NUMERO'] = df['GIH_NUMERO'].apply(lambda x: str(x).replace('.', ''))
-            guide_values = [str(value).replace('.', '') for value in guide_values]
-
-            # Filtre as colunas relevantes com base nos valores de guia
             df_filtered_guia = df[(df['GUIA_ATENDIMENTO'].isin(guide_values)) | (df['GUIA_CONTA'].isin(guide_values)) | (df['GIH_NUMERO'].isin(guide_values))]
-
-            df_filtered_guia = df_filtered_guia[df_filtered_guia['CTH_NUM'] == 1]
-            df_filtered_guia = df_filtered_guia[df_filtered_guia['GUIA_ATENDIMENTO'] == df_filtered_guia['GIH_NUMERO']]
-            df_filtered2 = df_filtered_guia[['HSP_NUM', 'HSP_PAC', 'CTH_NUM', 'FAT_SERIE', 'FAT_NUM', 'GUIA_ATENDIMENTO', 'GUIA_CONTA', 'GIH_NUMERO']]
-            st.dataframe(df_filtered2)
-            output2 = BytesIO()
-            df_filtered2.to_excel(output2, index=False)
-            output2.seek(0)
-            st.download_button(
-                label="Baixar arquivo Excel",
-                data=output2,
-                file_name=f"resultado_atendimentos_filtrado_{datetime.today().strftime('%Y-%m-%d')}.xlsx",
-                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-            )
         else:
-            st.write("Por favor, insira um valor para a coluna 'Guia' no filtro acima.")
+            df_filtered_guia = df.copy()
+
+        df_filtered_guia = df_filtered_guia[df_filtered_guia['CTH_NUM'] == 1]
+        df_filtered_guia = df_filtered_guia[df_filtered_guia['GUIA_ATENDIMENTO'] == df_filtered_guia['GIH_NUMERO']]
+        df_filtered2 = df_filtered_guia[['HSP_NUM', 'HSP_PAC', 'CTH_NUM', 'FAT_SERIE', 'FAT_NUM', 'GUIA_ATENDIMENTO', 'GUIA_CONTA', 'GIH_NUMERO']]
+
+        st.dataframe(df_filtered2)
+        output2 = BytesIO()
+        df_filtered2.to_excel(output2, index=False)
+        output2.seek(0)
+        st.download_button(
+            label="Baixar arquivo Excel",
+            data=output2,
+            file_name=f"resultado_atendimentos_filtrado_{datetime.today().strftime('%Y-%m-%d')}.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
     else:
         st.write("Por favor, faça o upload do arquivo ATENDIMENTOS v3.xls.")
+
 
 def page_tratamento():
     st.image("LOGO.png", width=150)
@@ -129,7 +135,10 @@ def page_tratamento():
             )
 
         if st.button('Aplicar Filtros'):
-            df_filtered = read_and_filter_xls(uploaded_file, column_names, guide_values if guia else None, date_range if data else None)
+            guide_values_to_use = guide_values if guia else None
+            date_range_to_use = date_range if data else None
+
+            df_filtered = read_and_filter_xls(uploaded_file, column_names, guide_values_to_use, date_range_to_use)
             if df_filtered is not None:
                 st.write('Tabela filtrada pelos valores selecionados:')
                 st.dataframe(df_filtered)
@@ -152,36 +161,35 @@ def page_tratamento():
     uploaded_file2 = st.file_uploader("Escolha o arquivo ATENDIMENTOS.xls", key="atendimentos")
 
     if uploaded_file2 is not None:
+        df = pd.read_excel(uploaded_file2)
+        
+        # Remover pontos dos valores nas colunas relevantes
+        df['GUIA_ATENDIMENTO'] = df['GUIA_ATENDIMENTO'].apply(lambda x: str(x).replace('.', ''))
+        df['GIH_NUMERO'] = df['GIH_NUMERO'].apply(lambda x: str(x).replace('.', ''))
+        guide_values = [str(value).replace('.', '') for value in guide_values]
+
         if guide_values:
-            df = pd.read_excel(uploaded_file2)
-            # Remova pontos dos valores nas colunas relevantes
-            df['GUIA_ATENDIMENTO'] = df['GUIA_ATENDIMENTO'].apply(lambda x: str(x).replace('.', ''))
-            df['GIH_NUMERO'] = df['GIH_NUMERO'].apply(lambda x: str(x).replace('.', ''))
-            guide_values = [str(value).replace('.', '') for value in guide_values]
-
             df_filtered_guia = df[df['GUIA_ATENDIMENTO'].isin(guide_values) | df['GIH_NUMERO'].isin(guide_values)]
-
-            df_filtered_guia = df_filtered_guia[df_filtered_guia['CTH_NUM'] == 0]
-            df_filtered_guia = df_filtered_guia[pd.notna(df_filtered_guia['FAT_NUM'])]
-            df_filtered_guia = df_filtered_guia[df_filtered_guia['GUIA_ATENDIMENTO'] == df_filtered_guia['GIH_NUMERO']]
-            # Mostra as colunas filtradas de interesse
-            df_filtered2 = df_filtered_guia[['HSP_NUM', 'HSP_PAC', 'CTH_NUM', 'FAT_SERIE', 'FAT_NUM', 'GUIA_ATENDIMENTO', 'GUIA_CONTA', 'GIH_NUMERO']]
-            st.dataframe(df_filtered2)
-
-            output2 = BytesIO()
-            df_filtered2.to_excel(output2, index=False)
-            output2.seek(0)
-            st.download_button(
-                label="Baixar arquivo Excel",
-                data=output2,
-                file_name=f"resultado_atendimentos_filtrado_{datetime.today().strftime('%Y-%m-%d')}.xlsx",
-                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-            )
         else:
-            st.write("Por favor, insira um valor para a coluna 'Guia' no filtro acima.")
+            df_filtered_guia = df.copy()
+
+        df_filtered_guia = df_filtered_guia[df_filtered_guia['CTH_NUM'] == 0]
+        df_filtered_guia = df_filtered_guia[pd.notna(df_filtered_guia['FAT_NUM'])]
+        df_filtered_guia = df_filtered_guia[df_filtered_guia['GUIA_ATENDIMENTO'] == df_filtered_guia['GIH_NUMERO']]
+        df_filtered2 = df_filtered_guia[['HSP_NUM', 'HSP_PAC', 'CTH_NUM', 'FAT_SERIE', 'FAT_NUM', 'GUIA_ATENDIMENTO', 'GUIA_CONTA', 'GIH_NUMERO']]
+
+        st.dataframe(df_filtered2)
+        output2 = BytesIO()
+        df_filtered2.to_excel(output2, index=False)
+        output2.seek(0)
+        st.download_button(
+            label="Baixar arquivo Excel",
+            data=output2,
+            file_name=f"resultado_atendimentos_filtrado_{datetime.today().strftime('%Y-%m-%d')}.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
     else:
         st.write("Por favor, faça o upload do arquivo ATENDIMENTOS v3.xls.")
-
 
 # Navegação entre páginas
 st.sidebar.title("Navegação")
