7a1ea880259c142ff2e704becfc51b1c4a773e17
PedronSol <pn77427@gmail.com>
2024-07-17 17:00:51 -0300

    "Inicio dos comentarios"

--- main.py
+++ main.py
@@ -1,100 +1,134 @@
+
+# Importação das bibliotecas responsáveis, respectivamente, por: tratamento de dados, 
+# criação e manutenção interface gráfica, tratamento de memória e gestão de arquivos e 
+# tratamento de valores de data
+
 import pandas as pd
 import streamlit as st
 from io import BytesIO
 from datetime import datetime
 
-primary_color = "#1f77b4"
-st.markdown(
-    f"""
-    <style>
-    .primary-color {{
-        background-color: {primary_color};
-        color: white;
-        border-radius: 4px;
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
+# Função responsável pela leitura dos arquivos em formato ".xls". Recebe como 
+# parâmetros o arquivo, o nome das colunas, o valor das guias e o intervalo de datas
 
 def read_and_filter_xls(file, column_names, guide_values, date_range):
+
+    # Criação do dataframe (espécie de tabela) a partir do arquivo fornecido
     df = pd.read_excel(file)
+
+    # Verifica se a coluna "Guia" encontra-se entre o nome das colunas do dataframe e houver valores de guia
     if 'Guia' in column_names and guide_values:
+        # Atualiza o dataframe para que contenha apenas os valores de guia 
         df = df[df['Guia'].astype(str).isin(guide_values)]
+
+    # Verifica se a coluna "Dt item" encontra-se entre o nome das colunas do dataframe e houver valores de data  
     if 'Dt item' in column_names and date_range:
+        # Converte os valores da coluna Dt item para formato datetime
         df['Dt item'] = pd.to_datetime(df['Dt item'], dayfirst=True, errors='coerce')
+        # Atualiza o dataframe para que contenha apenas os valores de data
         df = df[(df['Dt item'] >= pd.to_datetime(date_range[0])) & (df['Dt item'] <= pd.to_datetime(date_range[1]))]
+    
+    # Retorna o dataframe modificado
     return df
 
+# Função responsável por gerenciar a página principal da aplicação
 def main_page():
-    st.image("LOGO.png", width=150)
+    # Cria uma imagem com a logo fornecida, de tamanho 60px
+    st.image("LOGO.png", width=60)
+
+    # Cria um título para a seção
     st.header('Filtragem de dados sobre Internação')
 
+    # Cria um input que recebe um arquivo em formato xls
     uploaded_file = st.file_uploader("Escolha o arquivo 'Demonstrativo' em formato xls")
 
-    # st.markdown('<p class="primary-color">Este é um exemplo de texto com a cor primária.</p>', unsafe_allow_html=True)
-
+    # Cria uma variável com os nomes das colunas de interesse 
     column_names = ['Guia', 'Dt item']
+
+    # Cria uma variável vazia que receberá valores posteriormente
     guide_values = []
 
+    # Verifica se algum arquivo foi inserido na variável "uploaded_file"
     if uploaded_file is not None:
-        guia = st.checkbox("Filtro Guia", value=False)
+
+        # Cria uma variável associada a um checkbox que recebe valor "False" se a checkbox estiver desmarcada e vice-versa
+        guia = st.checkbox("Filtro Guia", value = False)
+
+        # Verifica o estado da variável "guia"
         if guia:
+            # Recupera o valor das guias inseridas em um text input e os separa por vírgulas em uma variável
             guide_values = st.text_input('Digite os valores das guias separados por vírgulas').split(',')
 
-        data = st.checkbox("Filtro Data", value=True)
+        # Cria uma variável associada a um checkbox que recebe valor "False" se a checkbox estiver desmarcada e vice-versa
+        data = st.checkbox("Filtro Data", value = True)
+
+        # Cria uma variável e associa a ela o valor "None"
         date_range = None
+
+        # Verifica o estado da variável "data"
         if data:
+            # Recupera o intervalo de data selecionado em um input em formato de calendário e o armazena em uma variável
             date_range = st.date_input(
                 "Selecione o intervalo de datas",
                 value=(datetime(2024, 1, 1).date(), datetime(2024, 12, 31).date())
             )
 
-        if st.button('Aplicar Filtros'):
-            guide_values_to_use = guide_values if guia else None
-            date_range_to_use = date_range if data else None
 
-            df_filtered = read_and_filter_xls(uploaded_file, column_names, guide_values_to_use, date_range_to_use)
-            if df_filtered is not None:
-             
-                if df_filtered.empty:
-                    st.markdown("<h3 style='color: red;'>Linha não encontrada</h3>", unsafe_allow_html=True)
-                else:
-                    st.write('Tabela filtrada pelos valores selecionados:')
-                    st.dataframe(df_filtered[['Guia', 'Dt item']])
-               
+        # Associa à variável "guide_values_to_use" o valor de "guide_values" se o filtro de guia estiver ativo, caso contrário será vazio
+        guide_values_to_use = guide_values if guia else None
 
-                    min_date = df_filtered['Dt item'].min()
-                    min_date2 = pd.to_datetime(min_date, format= "%Y-%m-%d %H:%M:%S")
-                    
-                    st.write(f"A menor data encontrada é: {min_date2}")
+        # Associa à variável "date_range_to_use" o valor de "date_range" se o filtro de data estiver ativo, caso contrário será vazio
+        date_range_to_use = date_range if data else None
 
-                    df_filtered_by_min_date = df_filtered[df_filtered['Dt item'] == min_date][['Guia', 'Dt item']]
-                # st.write('Tabela filtrada pela menor data:')
-                # st.dataframe(df_filtered_by_min_date)
+        # Modifica a variável "df_filtered" para receber o valor de uma planilha lida sob os parâmetros fornecidos
+        df_filtered = read_and_filter_xls(uploaded_file, column_names, guide_values_to_use, date_range_to_use)
+
+        # Condicional que verifica se a variável "df_filtered" é nula
+        if df_filtered is not None:
+            
+            # Verifica se a variável está vazia
+            if df_filtered.empty:
+                # Caso a variável esteja vazia, cria um elemento escrito na tela com as condições fornecidas
+                st.markdown("<h3 style='color: red;'>Digite um valor de guia válido</h3>", unsafe_allow_html=True)
+            else:
+                # Cria um elemento escrito na tela precedente ao display da tabela
+                st.write('Tabela filtrada pelos valores selecionados:')
+                # Cria graficamente a tabela cujas colunas são fonecidas entre os colchetes
+                st.dataframe(df_filtered[['Guia', 'Dt item']])
+            
+                # Resgata a menor data dentre os valores passados pelas filtragens
+                min_date = df_filtered['Dt item'].min()
+
+                # Converte o valor da variável "min_date" para datetime na formatação fornecida
+                min_date = pd.to_datetime(min_date, format= "%Y-%m-%d")
+                
+                # Cria um elemento escrito na tela que fornece o valor da variável "min_date"
+                st.write(f"A menor data encontrada é: {min_date}")
       
     else:
+        # Caso as condicionais não sejam atendidas, cria um elemento na tela que escreve o texto entre parênteses
         st.write("Por favor, faça o upload de um arquivo XLS/XLSX.")
 
+    # Cria um elemento de título com o texto entre as aspas
     st.header('Upload e Filtragem do Arquivo ATENDIMENTOS')
 
+    # Cria um input que recebe um arquivo em formato xls
     uploaded_file2 = st.file_uploader("Escolha o arquivo 'Atendimentos' no formato xls", key="atendimentos")
 
+    # Verifica se a variável "uploaded_file2" é vazia
     if uploaded_file2 is not None:
-        df = pd.read_excel(uploaded_file2)
-        df['Índice Original'] = df.index
-        df_filtered_guia = df[
-                df['GUIA_ATENDIMENTO'].isin(df.index) |
-                df['GUIA_CONTA'].isin(df.index) |
-                df['GIH_NUMERO'].isin(df.index)
-            ]
 
+        # Cria um dataframe a partir da leitura do arquivo recuperado pela variável "uploaded_file2"
+        df = pd.read_excel(uploaded_file2)
+
+        
         df['GUIA_ATENDIMENTO'] = df['GUIA_ATENDIMENTO'].astype(str).str.replace('.', '')
         df['GUIA_CONTA'] = df['GUIA_CONTA'].astype(str).str.replace('.', '')
         df['GIH_NUMERO'] = df['GIH_NUMERO'].astype(str).str.replace('.', '')
 
         if guide_values:
-            df_filtered_guia = df[df['GUIA_ATENDIMENTO'].isin(guide_values) | df['GUIA_CONTA'].isin(guide_values)]
+            df_filtered_guia =df['GUIA_ATENDIMENTO'].isin(guide_values) 
 
         else:
             df_filtered_guia = df.copy()
@@ -139,7 +173,7 @@ def main_page():
             st.dataframe(result)
 
             output2 = BytesIO()
-            df_filtered2.to_excel(output2, index=False)
+            result.to_excel(output2, index=False)
             output2.seek(0)
             st.download_button(
                 label="Baixar arquivo Excel",
@@ -147,8 +181,12 @@ def main_page():
                 file_name=f"resultado_atendimentos_filtrado_{datetime.today().strftime('%Y-%m-%d')}.xlsx",
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
-        else:
-            st.write("Por favor, primeiro aplique os filtros no arquivo XLS/XLSX.")
+        # else:
+        #     st.markdown(
+        #     "<h3 style='color: red; font-weight: bold;'>Por favor, primeiro clique em aplicar filtro</h3>",
+        #     unsafe_allow_html=True
+        #     )
+            
     else:
         st.write("Por favor, faça o upload do arquivo 'Atendimentos'!")
 
@@ -162,6 +200,7 @@ def page_tratamento():
     guide_values = []
 
     if uploaded_file is not None:
+        
         guia = st.checkbox("Filtro Guia", value=False)
         if guia:
             guide_values = st.text_input('Digite os valores das guias separados por vírgulas').split(',')
@@ -174,7 +213,7 @@ def page_tratamento():
                 value=(pd.to_datetime('2024-01-01').date(), pd.to_datetime('2024-12-31').date())
             )
 
-        if st.button('Aplicar Filtros'):
+        # if st.button('Aplicar Filtros'):
             guide_values_to_use = guide_values if guia else None
             date_range_to_use = date_range if data else None
 
@@ -211,22 +250,36 @@ def page_tratamento():
         else:
             df_filtered_guia = df.copy()
 
-        df_filtered_guia = df_filtered_guia[df_filtered_guia['CTH_NUM'] == 0]
-        df_filtered_guia = df_filtered_guia[df_filtered_guia['GUIA_ATENDIMENTO'] == df_filtered_guia['GIH_NUMERO']]
-        df_filtered2 = df_filtered_guia[['GUIA_ATENDIMENTO', 'GUIA_CONTA', 'HSP_NUM', 'HSP_PAC', 'CTH_NUM', 'FAT_SERIE', 'FAT_NUM', 'NFS_SERIE', 'NFS_NUMERO']]
-        df_filtered2['NFS_NUMERO'] = df_filtered2['NFS_NUMERO'].astype(str)
-        df_filtered2 = df_filtered2.rename(columns={'HSP_NUM':'IH', 'HSP_PAC':'REGISTRO', 'CTH_NUM':'CONTA', 'FAT_SERIE':'PRE.S', 'FAT_NUM':'PRE.NUM', 'NFS_SERIE':'FAT.S', 'NFS_NUMERO':'FAT.NUM'})
+        if 'guide_values_to_use' in locals() and 'df_filtered_guia' in locals() and 'df_filtered' in locals():
+
         
-        st.dataframe(df_filtered2)
-        output2 = BytesIO()
-        df_filtered2.to_excel(output2, index=False)                   
-        output2.seek(0)
-        st.download_button(
-            label="Baixar arquivo Excel",
-            data=output2,
-            file_name=f"resultado_atendimentos_filtrado_{datetime.today().strftime('%Y-%m-%d')}.xlsx",
-            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        )
+
+            df_filtered_guia = df_filtered_guia[df_filtered_guia['CTH_NUM'] == 0]
+            df_filtered_guia = df_filtered_guia[df_filtered_guia['GUIA_ATENDIMENTO'] == df_filtered_guia['GIH_NUMERO']]
+            df_filtered2 = df_filtered_guia[['GUIA_ATENDIMENTO', 'GUIA_CONTA', 'HSP_NUM', 'HSP_PAC', 'CTH_NUM', 'FAT_SERIE', 'FAT_NUM', 'NFS_SERIE', 'NFS_NUMERO']]
+            df_filtered2['NFS_NUMERO'] = df_filtered2['NFS_NUMERO'].astype(str)
+            df_filtered2 = df_filtered2.rename(columns={'HSP_NUM':'IH', 'HSP_PAC':'REGISTRO', 'CTH_NUM':'CONTA', 'FAT_SERIE':'PRE.S', 'FAT_NUM':'PRE.NUM', 'NFS_SERIE':'FAT.S', 'NFS_NUMERO':'FAT.NUM'})
+            result = pd.merge(df_filtered[['Guia', 'Dt item']], df_filtered2, left_on='Guia', right_on='GUIA_ATENDIMENTO', how='inner')
+                
+                
+            result.drop_duplicates(subset=['Guia'], keep='first', inplace=True)
+
+            st.dataframe(result)
+        
+            output2 = BytesIO()
+            result.to_excel(output2, index=False)                   
+            output2.seek(0)
+            st.download_button(
+                label="Baixar arquivo Excel",
+                data=output2,
+                file_name=f"resultado_atendimentos_filtrado_{datetime.today().strftime('%Y-%m-%d')}.xlsx",
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            ) 
+        else:
+            st.markdown(
+            "<h3 style='color: red; font-weight: bold;'>Por favor, primeiro clique em aplicar filtro</h3>",
+            unsafe_allow_html=True
+            )
     else:
         st.write("Por favor, faça o upload do arquivo ATENDIMENTOS v3.xls.")
 
--- teste_tkinter.py
+++ teste_tkinter.py
@@ -9,14 +9,14 @@ class App(customtkinter.CTk):
         self.geometry("1000x650")
 
         # Adiciona a imagem da logo do hospital
-        my_image = customtkinter.CTkImage(light_image=Image.open(r"C:\Users\pedronobrega\Desktop\Ler_Planilhas_Novo\LOGO.png"), dark_image=Image.open(r"C:\Users\pedronobrega\Desktop\Ler_Planilhas_Novo\LOGO.png"), size=(85, 85))
+        my_image = customtkinter.CTkImage(light_image=Image.open(r"C:\Users\pedronobrega\Desktop\Ler_Planilhas_Novo\LOGO.png"), dark_image=Image.open(r"C:\Users\pedronobrega\Desktop\Ler_Planilhas_Novo\LOGO.png"), size=(75, 75))
         
         # Cria um botão com a imagem da logo que redireciona para o site do hospital
         botao_logo = customtkinter.CTkButton(self, text=None, image=my_image, height=60, fg_color='transparent', width=60, hover=False, anchor='nw')
-        botao_logo.place(x=30, y=20)
+        botao_logo.place(x=30, y=30)
 
         # Cria a label referente ao título da página
-        label_principal = customtkinter.CTkLabel(self, text = "LEITOR DE PLANILHAS", fg_color="transparent", height=130, font = ("Arial Rounded MT Bold", 30, "bold"))
+        label_principal = customtkinter.CTkLabel(self, text = "LEITOR DE PLANILHAS", fg_color="transparent", height=130, font = ("Arial Rounded MT Bold", 35, "bold"))
         label_principal.pack()
 
         # Label "Filtragem de dados sobre internação"
@@ -24,7 +24,8 @@ class App(customtkinter.CTk):
         label_primeira_filtragem.place(x=30, y = 150)
 
         # Label instrutiva para inserção de arquivo "Insira o arquivo Demonstrativo"
-        label_demonstrativo = ""
+        label_demonstrativo = customtkinter.CTkLabel(self, text = "Escolha o arquivo 'Demonstrativo' em formato xls", fg_color='transparent', font = ("Helvetica", 14, "bold"), text_color='gray')
+        label_demonstrativo.place(x = 30, y = 185)
 
 
     # add methods to app
